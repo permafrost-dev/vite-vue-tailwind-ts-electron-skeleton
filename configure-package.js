1c7fc60629205f7162daab5a1d7d62a5f675b6d7
Patrick Organ <patrick@permafrost.dev>
2022-02-05 16:16:06 -0500

    use newer version of configure-package

--- configure-package.js
+++ configure-package.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
@@ -10,6 +11,7 @@ const path = require('path');
 const readline = require('readline');
 const util = require('util');
 const cp = require('child_process');
+const { basename } = require('path');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -23,7 +25,8 @@ const basePath = __dirname;
 const packageInfo = {
     name: '',
     description: '',
-    org: {
+    vendor: {
+        name: '',
         github: '',
     },
     author: {
@@ -31,30 +34,45 @@ const packageInfo = {
         email: '',
         github: '',
     },
-    vendor: {
-        name: '',
-        email: '',
-        github: '',
-    },
+};
+
+const runCommand = str => {
+    cp.execSync(str, { cwd: __dirname, encoding: 'utf-8', stdio: 'inherit' });
+};
+
+const gitCommand = command => {
+    return cp.execSync(`git ${command}`, { env: process.env, cwd: __dirname, encoding: 'utf-8', stdio: 'pipe' }) || '';
 };
 
 const installDependencies = () => {
     cp.execSync('npm install', { cwd: __dirname, encoding: 'utf-8', stdio: 'inherit' });
 };
 
-const askQuestion = async prompt => {
+const askQuestion = async (prompt, defaultValue = '') => {
     let result = '';
 
     try {
-        result = await question(`${prompt} `);
+        result = await question(`${prompt} ${defaultValue.length ? '(' + defaultValue + ') ' : ''}`);
     } catch (err) {
         result = false;
     }
 
     return new Promise(resolve => {
+        if (!result || result.trim().length === 0) {
+            result = defaultValue;
+        }
+
         resolve(result);
     });
 };
+
+function rescue(func, defaultValue = null) {
+    try {
+        return func();
+    } catch (e) {
+        return defaultValue;
+    }
+}
 
 function is_dir(path) {
     try {
@@ -66,21 +84,28 @@ function is_dir(path) {
     }
 }
 
+function is_symlink(path) {
+    return rescue(() => fs.lstatSync(path).isSymbolicLink(), false);
+}
+
+function is_file(path) {
+    return rescue(() => fs.lstatSync(path).isFile(), false);
+}
+
 const replaceVariablesInFile = (filename, packageInfo) => {
     let content = fs.readFileSync(filename, { encoding: 'utf-8' }).toString();
     const originalContent = content.slice();
 
     content = content
         .replace(/package-skeleton/g, packageInfo.name)
+        .replace(/\{\{vendor\.name\}\}/g, packageInfo.vendor.name)
+        .replace(/\{\{vendor\.github\}\}/g, packageInfo.vendor.github)
         .replace(/\{\{package\.name\}\}/g, packageInfo.name)
         .replace(/\{\{package\.description\}\}/g, packageInfo.description)
         .replace(/\{\{package\.author\.name\}\}/g, packageInfo.author.name)
         .replace(/\{\{package\.author\.email\}\}/g, packageInfo.author.email)
         .replace(/\{\{package\.author\.github\}\}/g, packageInfo.author.github)
-        .replace(/\{\{package\.org\.github\}\}/g, packageInfo.org.github)
-        .replace(/\{\{package\.vendor\.name\}\}/g, packageInfo.vendor.name)
-        .replace(/\{\{package\.vendor\.email\}\}/g, packageInfo.vendor.email)
-        .replace(/\{\{package\.vendor\.github\}\}/g, packageInfo.vendor.github)
+        .replace(/\{\{date\.year\}\}/g, new Date().getFullYear())
         .replace('Template Setup: run `node configure-package.js` to configure.\n', '');
 
     if (originalContent != content) {
@@ -94,6 +119,7 @@ const processFiles = (directory, packageInfo) => {
             '.',
             '..',
             '.git',
+            '.github',
             '.editorconfig',
             '.gitattributes',
             '.gitignore',
@@ -118,16 +144,30 @@ const processFiles = (directory, packageInfo) => {
             return;
         }
 
-        replaceVariablesInFile(fqName, packageInfo);
+        if (is_file(fqName)) {
+            try {
+                replaceVariablesInFile(fqName, packageInfo);
+            } catch (err) {
+                console.log(`error processing file ${relativeName}`);
+            }
+        }
     });
 };
 
-const conditionalAsk = async (obj, propName, onlyEmpty, prompt) => {
+const conditionalAsk = async (obj, propName, onlyEmpty, prompt, allowEmpty = false, alwaysAsk = true) => {
     const value = obj[propName];
 
-    if (!onlyEmpty || !value.length) {
-        while (obj[propName].length === 0) {
-            obj[propName] = await askQuestion(prompt);
+    if (!onlyEmpty || !value.length || alwaysAsk) {
+        while (obj[propName].length === 0 || alwaysAsk) {
+            obj[propName] = await askQuestion(prompt, value);
+
+            if (allowEmpty && obj[propName].length === 0) {
+                break;
+            }
+
+            if (obj[propName].length > 0) {
+                break;
+            }
         }
     }
 
@@ -135,26 +175,274 @@ const conditionalAsk = async (obj, propName, onlyEmpty, prompt) => {
 };
 
 const populatePackageInfo = async (onlyEmpty = false) => {
-    await conditionalAsk(packageInfo, 'name', onlyEmpty, 'package name?');
+    const remoteUrlParts = gitCommand('config remote.origin.url').trim()
+        .replace(':', '/')
+        .split('/');
+
+    console.log();
+
+    packageInfo.name = basename(__dirname);
+    packageInfo.author.name = gitCommand('config user.name').trim();
+    packageInfo.author.email = gitCommand('config user.email').trim();
+    packageInfo.vendor.name = packageInfo.author.name;
+    packageInfo.author.github = remoteUrlParts[1];
+    packageInfo.vendor.github = remoteUrlParts[1];
+
+    await conditionalAsk(packageInfo, 'name', onlyEmpty, 'package name?', false);
     await conditionalAsk(packageInfo, 'description', onlyEmpty, 'package description?');
     await conditionalAsk(packageInfo.author, 'name', onlyEmpty, 'author name?');
     await conditionalAsk(packageInfo.author, 'email', onlyEmpty, 'author email?');
     await conditionalAsk(packageInfo.author, 'github', onlyEmpty, 'author github username?');
-    await conditionalAsk(packageInfo.vendor, 'name', onlyEmpty, 'vendor name?');
-    await conditionalAsk(packageInfo.vendor, 'email', onlyEmpty, 'vendor contact email?');
-    await conditionalAsk(packageInfo.vendor, 'github', onlyEmpty, 'vendor github username?');
+    await conditionalAsk(packageInfo.vendor, 'name', onlyEmpty, 'vendor name (default is author name)?', true);
+    await conditionalAsk(packageInfo.vendor, 'github', onlyEmpty, 'vendor github org/user name (default is author github)?', true);
 
-    packageInfo.org.github = packageInfo.name.split('/').shift();
+    if (packageInfo.vendor.name.length === 0) {
+        packageInfo.vendor.name = packageInfo.author.name;
+    }
+
+    if (packageInfo.vendor.github.length === 0) {
+        packageInfo.vendor.github = packageInfo.author.github;
+    }
+};
+
+const safeUnlink = path => fs.existsSync(path) && fs.unlinkSync(path);
+const getWorkflowFilename = name => `${__dirname}/.github/workflows/${name}.yml`;
+const getGithubConfigFilename = name => `${__dirname}/.github/${name}.yml`;
+
+class Features {
+    codecov = {
+        prompt: 'Use code coverage service codecov?',
+        enabled: true,
+        dependsOn: [],
+        disable: () => {
+            const testsWorkflowFn = getWorkflowFilename('run-tests');
+            const contents = fs.readFileSync(testsWorkflowFn, { encoding: 'utf-8' });
+
+            fs.writeFileSync(testsWorkflowFn, contents.replace('USE_CODECOV_SERVICE: yes', 'USE_CODECOV_SERVICE: no'), {
+                encoding: 'utf-8',
+            });
+            safeUnlink(getGithubConfigFilename('codecov'));
+        },
+    };
+
+    dependabot = {
+        prompt: 'Use Dependabot?',
+        enabled: true,
+        dependsOn: [],
+        disable: () => {
+            safeUnlink(getGithubConfigFilename('dependabot'));
+            this.automerge.disable();
+        },
+    };
+
+    automerge = {
+        prompt: 'Automerge Dependabot PRs?',
+        enabled: true,
+        dependsOn: ['dependabot'],
+        disable: () => {
+            safeUnlink(getWorkflowFilename('dependabot-auto-merge'));
+        },
+    };
+
+    codeql = {
+        prompt: 'Use CodeQL Quality Analysis?',
+        enabled: true,
+        dependsOn: [],
+        disable: () => {
+            safeUnlink(getWorkflowFilename('codeql-analysis'));
+        },
+    };
+
+    updateChangelog = {
+        prompt: 'Use Changelog Updater Workflow?',
+        enabled: true,
+        dependsOn: [],
+        disable: () => {
+            safeUnlink(getWorkflowFilename('update-changelog'));
+        },
+    };
+
+    useMadgePackage = {
+        prompt: 'Use madge package for code analysis?',
+        enabled: true,
+        dependsOn: [],
+        disable: () => {
+            runCommand('npm rm madge');
+            safeUnlink(`${__dirname}/.madgerc`);
+
+            const pkg = require(`${__dirname}/package.json`);
+
+            delete pkg.scripts['analyze:deps:circular'];
+            delete pkg.scripts['analyze:deps:list'];
+            delete pkg.scripts['analyze:deps:graph'];
+
+            fs.writeFileSync(`${__dirname}/package.json`, JSON.stringify(pkg, null, 4), { encoding: 'utf-8' });
+        },
+    };
+
+    features = [this.codecov, this.dependabot, this.automerge, this.codeql, this.updateChangelog, this.useMadgePackage];
+
+    async run() {
+        for (let feature of this.features) {
+            if (feature.enabled) {
+                feature.enabled = await askBooleanQuestion(feature.prompt, feature.default);
+            }
+
+            if (!feature.enabled) {
+                feature.disable();
+            }
+        }
+    }
+}
+
+function dedent(templ, ...values) {
+    let strings = Array.from(typeof templ === 'string' ? [templ] : templ);
+    strings[strings.length - 1] = strings[strings.length - 1].replace(/\r?\n([\t ]*)$/, '');
+    const indentLengths = strings.reduce((arr, str) => {
+        const matches = str.match(/\n([\t ]+|(?!\s).)/g);
+        if (matches) {
+            return arr.concat(
+                matches.map(match => {
+                    var _a;
+                    return ((_a = match.match(/[\t ]/g)) == null ? void 0 : _a.length) ?? 0;
+                }),
+            );
+        }
+        return arr;
+    }, []);
+    if (indentLengths.length) {
+        const pattern = new RegExp(`[	 ]{${Math.min(...indentLengths)}}`, 'g');
+        strings = strings.map(str => str.replace(pattern, '\n'));
+    }
+    strings[0] = strings[0].replace(/^\r?\n/, '');
+    let string = strings[0];
+    values.forEach((value, i) => {
+        const endentations = string.match(/(?:^|\n)( *)$/);
+        const endentation = endentations ? endentations[1] : '';
+        let indentedValue = value;
+        if (typeof value === 'string' && value.includes('\n')) {
+            indentedValue = String(value)
+                .split('\n')
+                .map((str, i2) => (i2 === 0 ? str : `${endentation}${str}`))
+                .join('\n');
+        }
+        string += indentedValue + strings[i + 1];
+    });
+    return string;
+}
+
+class OptionalPackages {
+    config = {
+        prompt: 'Use a yaml config file?',
+        enabled: true,
+        default: false,
+        dependsOn: [],
+        name: 'conf',
+        add: () => {
+            cp.execSync('npm install conf js-yaml', { cwd: __dirname, stdio: 'inherit' });
+            fs.writeFileSync(`${__dirname}/config.yaml`, '', { encoding: 'utf-8' });
+
+            fs.writeFileSync(
+                `${__dirname}/src/config.ts`,
+                `
+                import Conf from 'conf';
+                import yaml from 'js-yaml';
+
+                const ConfBaseConfig = {
+                    cwd: __dirname,
+                    deserialize: (text: string) => yaml.load(text),
+                    serialize: value => yaml.dump(value, { indent: 2 }),
+                    fileExtension: 'yml',
+                };
+
+                export function createConf(name: string, options: Record<string, any> = {}): Conf {
+                    return new Conf(<any>{
+                        configName: name,
+                        ...Object.assign({}, ConfBaseConfig, options),
+                    });
+                }
+            `.trim(),
+                { encoding: 'utf-8' },
+            );
+        },
+    };
+
+    dotenv = {
+        prompt: 'Use a .env file?',
+        enabled: true,
+        default: false,
+        dependsOn: [],
+        name: 'dotenv',
+        add: () => {
+            runCommand('npm', ['install', 'dotenv'], { cwd: __dirname, stdio: 'inherit' });
+
+            fs.mkdirSync(`${__dirname}/dist`, { recursive: true });
+            fs.writeFileSync(`${__dirname}/dist/.env`, 'TEST_VALUE=1\n', { encoding: 'utf-8' });
+            fs.writeFileSync(
+                `${__dirname}/src/init.ts`,
+                `
+                require('dotenv').config({ path: \`\${__dirname}/.env' })
+            `.trim(),
+                { encoding: 'utf-8' },
+            );
+        },
+    };
+
+    otherPackages = {
+        prompt: 'Comma-separated list of packages to install:',
+        enabled: true,
+        default: '',
+        dependsOn: [],
+        name: 'otherPackages',
+        add: values => {
+            cp.execSync('npm install ' + values.join(' '), { cwd: __dirname, stdio: 'inherit' });
+        },
+    };
+
+    optionalPackages = [this.config, this.dotenv];
+
+    async run() {
+        for (let pkg of this.optionalPackages) {
+            const result = await askBooleanQuestion(pkg.prompt, pkg.default);
+            if (result) {
+                pkg.add();
+            }
+        }
+
+        const packageList = await askQuestion(this.otherPackages, this.otherPackages.default);
+
+        if (packageList.length > 0) {
+            this.otherPackages.add(packageList.split(',').map(pkg => pkg.trim()));
+        }
+
+        cp.execSync('node ./node_modules/.bin/prettier --write ./src', { cwd: __dirname, stdio: 'inherit' });
+        cp.execSync('node ./node_modules/.bin/eslint --fix ./src', { cwd: __dirname, stdio: 'inherit' });
+    }
+}
+
+async function configureOptionalFeatures() {
+    await new Features().run();
+}
+
+const askBooleanQuestion = async str => {
+    const resultStr = await askQuestion(`${str} `);
+    const result = resultStr.toString().toLowerCase()
+        .replace(/ /g, '')
+        .replace(/[^yn]/g, '')
+        .slice(0, 1);
+
+    return result === 'y';
 };
 
 const run = async function () {
     await populatePackageInfo();
+    await configureOptionalFeatures();
 
-    const confirm = (await askQuestion('Process files (this will change content of some files!)? '))
+    const confirm = (await askQuestion('Process files (this will modify files)? '))
         .toString()
         .toLowerCase()
         .replace(/ /g, '')
-        .replace(/[^yesno]/g, '')
+        .replace(/[^yn]/g, '')
         .slice(0, 1);
 
     if (confirm !== 'y') {
@@ -163,10 +451,21 @@ const run = async function () {
         return;
     }
 
-    processFiles(__dirname, packageInfo);
+    try {
+        processFiles(__dirname, packageInfo);
+        installDependencies();
+        await new OptionalPackages().run();
+    } catch (err) {
+        //
+    }
+
     rl.close();
 
-    installDependencies();
+    console.log('Done, removing this script.');
+    fs.unlinkSync(__filename);
+
+    runCommand('git add .');
+    runCommand('git commit -m"commit configured package files"');
 };
 
 run();
