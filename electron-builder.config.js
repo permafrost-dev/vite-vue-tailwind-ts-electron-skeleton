11ffa1ffe96703863b84d0068a7244f0f8e4ecb3
patinthehat <patinthehat@users.noreply.github.com>
2022-02-05 21:19:05 +0000

    Fix styling

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -1,6 +1,8 @@
 if (process.env.VITE_APP_VERSION === undefined) {
-  const now = new Date;
-  process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
+    const now = new Date();
+    process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${
+        now.getUTCHours() * 60 + now.getUTCMinutes()
+    }`;
 }
 
 /**
@@ -8,17 +10,14 @@ if (process.env.VITE_APP_VERSION === undefined) {
  * @see https://www.electron.build/configuration/configuration
  */
 const config = {
-  directories: {
-    output: 'dist',
-    buildResources: 'buildResources',
-  },
-  files: [
-    'packages/renderer/src/index.ts',
-    'packages/**/dist/**',
-  ],
-  extraMetadata: {
-    version: process.env.VITE_APP_VERSION,
-  },
+    directories: {
+        output: 'dist',
+        buildResources: 'buildResources',
+    },
+    files: ['packages/renderer/src/index.ts', 'packages/**/dist/**'],
+    extraMetadata: {
+        version: process.env.VITE_APP_VERSION,
+    },
 };
 
 module.exports = config;
--- src/main.js
+++ src/main.js
@@ -4,6 +4,4 @@ import router from './router';
 import store from './store';
 import './tailwind.css';
 
-createApp(App).use(router)
-    .use(store)
-    .mount('#app');
+createApp(App).use(router).use(store).mount('#app');
